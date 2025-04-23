fa4c1ab6583af3e88892c498679af0aeb5ee9ce0
김한솔 <studio.gliburch@gmail.com>
2025-04-23 15:29:13 +0900

    Vercel 배포를 위한 기본 설정을 추가한다

--- server.js
+++ server.js
@@ -9,10 +9,18 @@ fastify.get('/', async function handler (request, reply) {
   return { hello: 'world' }
 })
 
-// Run the server!
-try {
-  await fastify.listen({ port: 3000 })
-} catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
+// For local development
+if (process.env.NODE_ENV === 'development') {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+// Export for serverless use
+export default async (req, res) => {
+  await fastify.ready()
+  fastify.server.emit('request', req, res)
 }
